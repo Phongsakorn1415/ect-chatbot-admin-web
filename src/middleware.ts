1222659af58da2b81ad3fe823c83d09df69237fe
Phongsakorn <s6603052411032@email.kmutnb.ac.th>
2025-11-02 12:55:01 +0700

    สร้างหน้า page register สำหรับให้ผู้ที่ได้รับคำเชิญสมัคร

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,15 @@ export async function middleware(req: any) {
     return NextResponse.redirect(new URL("/admin", req.url));
   }
 
+  //check if invite token is present for register page
+  // if (pathname === "/register") {
+  //   const url = req.nextUrl.clone();
+  //   const token = url.searchParams.get("token");
+  //   if (!token) {
+  //     return NextResponse.redirect(new URL("/", req.url));
+  //   }
+  // }
+
   // Account management page access control
   if (pathname.startsWith("/admin/accounts") && !user) {
     return NextResponse.redirect(new URL("/", req.url));
